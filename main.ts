255f41a47facbb84921b52c46db4c1f78ab891bd
hiso3931 <hiso3931@elev.harstad.kommune.no>
2024-06-12 13:06:43 +0200

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,20 @@
+let retning = input.compassHeading()
+if (retning < 22.5) {
+    basic.showArrow(ArrowNames.North)
+} else if (retning < 67.5) {
+    basic.showArrow(ArrowNames.NorthWest)
+} else if (retning < 112.5) {
+    basic.showArrow(ArrowNames.West)
+} else if (retning < 157.5) {
+    basic.showArrow(ArrowNames.SouthWest)
+} else if (retning < 202.5) {
+    basic.showArrow(ArrowNames.South)
+} else if (retning < 247.5) {
+    basic.showArrow(ArrowNames.SouthEast)
+} else if (retning < 292.5) {
+    basic.showArrow(ArrowNames.East)
+} else if (retning < 337.5) {
+    basic.showArrow(ArrowNames.NorthEast)
+} else {
+    basic.showArrow(ArrowNames.North)
+}
